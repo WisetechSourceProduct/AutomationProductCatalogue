eb57e9f14c2277625cb09d49cc71439785c939ce
HariharanWTS <hariharanwisetechsource@gmail.com>
2024-03-25 15:29:39 +0530

    second page update

--- wiseProductCatalog/wiseProductCatalogApp/views.py
+++ wiseProductCatalog/wiseProductCatalogApp/views.py
@@ -5,11 +5,11 @@ import pandas as pd
 def home(request):
 
     df = pd.read_excel("https://github.com/WisetechSourceProduct/AutomationProductCatalogue/raw/main/wiseProductCatalog/static/required_documents/wiseProductCatalogContentSheet.xlsx", header=None)
-    
     content = dict(zip(df[0], df[1])) # Convert excel file into dict format
-    
     return render(request, "wiseProductCatalogApp/home.html",{"excel_data":content} )
 
 
 def products(request):
-    return render(request,"wiseProductCatalogApp/products.html")
+    df = pd.read_excel("https://github.com/WisetechSourceProduct/AutomationProductCatalogue/raw/main/wiseProductCatalog/static/required_documents/wiseProductCatalogContentSheet.xlsx", header=None)
+    content = dict(zip(df[0], df[1])) # Convert excel file into dict format
+    return render(request,"wiseProductCatalogApp/products.html",{"excel_data":content})
