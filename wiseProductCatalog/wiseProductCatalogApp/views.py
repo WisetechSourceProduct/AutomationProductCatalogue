9cf56821f714e8de9d61f85a57691ed395f5359b
HariharanWTS <hariharanwisetechsource@gmail.com>
2024-03-26 10:10:21 +0530

    background color and footer changes

--- wiseProductCatalog/wiseProductCatalogApp/views.py
+++ wiseProductCatalog/wiseProductCatalogApp/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 import pandas as pd
 
+excel_file_path = "https://github.com/WisetechSourceProduct/AutomationProductCatalogue/raw/main/wiseProductCatalog/static/required_documents/wiseProductCatalogContentSheet.xlsx"
 # Create your views here.
 def home(request):
 
-    df = pd.read_excel("https://github.com/WisetechSourceProduct/AutomationProductCatalogue/raw/main/wiseProductCatalog/static/required_documents/wiseProductCatalogContentSheet.xlsx", header=None)
+    df = pd.read_excel(excel_file_path, header=None)
     content = dict(zip(df[0], df[1])) # Convert excel file into dict format
     return render(request, "wiseProductCatalogApp/home.html",{"excel_data":content} )
 
 
 def products(request):
-    df = pd.read_excel("https://github.com/WisetechSourceProduct/AutomationProductCatalogue/raw/main/wiseProductCatalog/static/required_documents/wiseProductCatalogContentSheet.xlsx", header=None)
+    df = pd.read_excel(excel_file_path, header=None)
     content = dict(zip(df[0], df[1])) # Convert excel file into dict format
     print(content)
     return render(request,"wiseProductCatalogApp/products.html",{"excel_data":content})
