c8bf2926e0bcd3f816d8f79d28252399b0e43209
HariharanWTS <hariharanwisetechsource@gmail.com>
2024-04-03 13:01:42 +0530

    views update

--- wiseProductCatalog/wiseProductCatalogApp/views.py
+++ wiseProductCatalog/wiseProductCatalogApp/views.py
@@ -6,14 +6,46 @@ excel_file_path = "https://github.com/WisetechSourceProduct/AutomationProductCat
 
 
 def excel_sheet_finder(excel_file):
+
     df = pd.ExcelFile(excel_file_path)
     return df.sheet_names # Returns sheet names as list
 
 
 def dict_maker(excel_file): # function for convert excel file to dictionary
+
     df = pd.read_excel(excel_file, header=None)
     content = dict(zip(df[0], df[1]))
     return content
+
+
+def products_dict_maker():
+
+    products_dict = {}
+    products_logo_dict = {}
+    excel_header = []
+
+    products_dataframe = pd.read_excel(excel_file_path, sheet_name="Products")
+    for i in products_dataframe:
+        excel_header.append(i)
+
+    products_dataframe_fillna = products_dataframe.fillna("None")
+    products_dataframe_fillna[excel_header[1]] =  products_dataframe[excel_header[1]].ffill() # it fills NaN value with previous row data
+
+    for index, row in products_dataframe_fillna.iterrows():
+        if row[excel_header[1]].rstrip(" \n") not in products_dict:
+            if row[excel_header[2]] != "None":
+                products_logo_dict[row[excel_header[1]].rstrip(" \n")] = row[excel_header[2]] # Coding for logo dictionary
+            products_dict[row[excel_header[1]].rstrip(" \n")] = {}
+            products_dict[row[excel_header[1]].rstrip(" \n")][row[excel_header[4]]] = [*row[5:]]
+        else:
+            products_dict[row[excel_header[1]].rstrip(" \n")][row[excel_header[4]]] = [*row[5:]]
+
+    #print(products_dict)
+    #print(products_logo_dict)
+
+    return products_dict, products_logo_dict
+    
+
 
 
 # Create your views here.
@@ -37,6 +69,8 @@ def home(request):
 
 def products(request):
 
+    """
+
     products_dict = {}
     products_logo_dict = {}
     excel_header = []
@@ -58,8 +92,11 @@ def products(request):
             products_dict[row[excel_header[1]].rstrip(" \n")][row[excel_header[4]]] = [*row[5:]]
         else:
             products_dict[row[excel_header[1]].rstrip(" \n")][row[excel_header[4]]] = [*row[5:]]
-    #print(products_dict)
+    print(products_dict)
     #print(products_logo_dict)
+    """
+    products_dictionary = products_dict_maker()
+    content = dict_maker(excel_file_path) # Convert excel file into dict format
 
     """        
     for i in products_dict:
@@ -67,8 +104,13 @@ def products(request):
             print(products_dict[i][k][1])
     """
 
-    return render(request,"wiseProductCatalogApp/products.html",{"excel_data":content, "main_sector":products_dict.keys(), "main_sector_logo":products_logo_dict})
+    return render(request,"wiseProductCatalogApp/products.html",{"excel_data":content, "main_sector":products_dictionary[0].keys(), "main_sector_logo":products_dictionary[1]})
 
 
-def subproducts(request):
-    return render(request,"wiseProductCatalogApp/subproducts.html")
+def subproducts(request, product_name):
+
+    content = dict_maker(excel_file_path) # Convert excel file into dict format
+    products_dictionary = products_dict_maker()
+    #print(products_dictionary[0].get(product_name)) 
+    subproducts_name = products_dictionary[0].get(product_name) # Have to handle error on this area. If the key not found what will be the web page design
+    return render(request,"wiseProductCatalogApp/subproducts.html", {"excel_data":content, "subproducts":subproducts_name})
