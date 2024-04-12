ca76857a41542c58e64a12715e8891e9c5d75461
HariharanWTS <hariharanwisetechsource@gmail.com>
2024-04-12 17:08:26 +0530

    logo update

--- wiseProductCatalog/wiseProductCatalogApp/urls.py
+++ wiseProductCatalog/wiseProductCatalogApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('products/',views.products,name = "products"),
-    path('subproducts/<str:product_name>/',views.subproducts,name = "subproducts"),
+    path('products/<str:product_name>/',views.subproducts,name = "subproducts"),
     path('productdetails',views.productdetails,name = "productdetails"),
 ]
